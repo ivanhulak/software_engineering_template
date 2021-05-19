e779869efb48643d713553cf75f8a5961c1fbb0e
ivanhulak <g.ivan.gulak17@gmail.com>
2021-05-19 12:21:06 +0300

    Try to add js-doc

--- src/ex1.js
+++ src/ex1.js
@@ -1,3 +1,9 @@
+/**
+* Создаём объект person 
+* @param {string} name Имя
+* @param {number} age Возраст
+* @param {function} greet Функция, которая выводит в консоль слово Greet
+*/
 const person = new Object({
 	name: 'Maxim',
 	age: 25,
